6deb6d83784cb596c4dc0004f1a2d800c3eeddbb
dimitrisanastasiadis <56549720+dimitrisanastasiadis@users.noreply.github.com>
2019-11-06 22:03:55 +0200

    Project job ads

---
+++ job_ads_anastasiadis/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data.SqlClient;
+
+using System.Data;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(sender.ToString(), "Button: ");
+        }
+        public static SqlConnection Get_DB_Connection()
+
+        {
+
+            string cn_String = Properties.Settings1.Default.connection_String;
+
+            SqlConnection cn_connection = new SqlConnection(cn_String);
+
+            if (cn_connection.State != ConnectionState.Open) cn_connection.Open();
+
+
+            return cn_connection;
+
+        }
+
+        private void Insert_User(object sender, RoutedEventArgs e)
+        {
+           
+                try
+                {
+                SqlConnection cn_connection = Get_DB_Connection();
+
+
+                
+
+                SqlCommand cmd_Command = new SqlCommand("insert into user_table(username, password) values('kostas','123')", cn_connection);
+
+                cmd_Command.ExecuteNonQuery();
+                
+            }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            
+        }
+
+
+    }
+}
