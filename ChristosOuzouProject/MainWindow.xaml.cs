cdc11762b46a1907eb50207eb207134a6ca51047
Christos Ouzounidis <chrisagioneri@yahoo.gr>
2019-11-09 18:45:37 +0200

    insert jobs ads v1

--- ChristosOuzouProject/MainWindow.xaml.cs
+++ ChristosOuzouProject/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace ChristosOuzouProject
 {
     /// <summary>
@@ -20,9 +24,56 @@ namespace ChristosOuzouProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        readonly string constring = "SERVER=localhost;DATABASE=mydb;UID=christos;PASSWORD=2341093066;";
         public MainWindow()
         {
-            InitializeComponent();
+            //InitializeComponent();
+            /*string constring = "SERVER=localhost;DATABASE=mydb;UID=christos;PASSWORD=2341093066;";
+            StringBuilder errorMessages = new StringBuilder();
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(constring);
+                conn.Open();
+                MySqlCommand comm1 = new MySqlCommand("INSERT INTO users VALUES('USRNAM','PASS','SURNAM','FATHER','1900-1-1','ROLE')", conn);
+                comm1.ExecuteNonQuery();
+                MySqlCommand comm = new MySqlCommand("SELECT * FROM users", conn);
+
+                DataTable dt = new DataTable();
+                dt.Load(comm.ExecuteReader());
+                dtgrid.DataContext = dt;
+                conn.Close();
+            }
+            catch (MySqlException ex)
+            {
+                
+                System.Diagnostics.Debug.WriteLine(errorMessages.ToString());
+            }*/
+           
+        }
+        private void ComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> data = new List<string>();
+            try
+            {
+                MySqlConnection conn = new MySqlConnection(constring);
+                conn.Open();
+                MySqlCommand comm = new MySqlCommand("SELECT DISTINCT name FROM subcategories WHERE catId=1", conn);
+                MySqlDataReader rdr = comm.ExecuteReader();
+                while (rdr.Read())
+                    data.Add(rdr["name"].ToString());
+                rdr.Close();
+                var comboBox = sender as ComboBox;
+                comboBox.ItemsSource = data;
+
+            }
+            catch (MySqlException ex)
+            {
+
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
         }
     }
+
 }
+    
+
