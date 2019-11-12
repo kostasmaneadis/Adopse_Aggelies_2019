4de08c06ee8ea26c08e24199e3690d13df22a3f4
kostasmaneadis <kmaneadis@gmail.com>
2019-11-12 11:06:30 +0200

    First UI and db tests

---
+++ KonstantinosManeadis/Register_page.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KonstantinosManeadis
+{
+    /// <summary>
+    /// Interaction logic for Register_page.xaml
+    /// </summary>
+    public partial class Register_page : Page
+    {
+        public Register_page()
+        {
+            InitializeComponent();
+        }
+    }
+}
