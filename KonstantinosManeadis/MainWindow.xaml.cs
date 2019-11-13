3327f60f9f66bc3e170db29f3a1a82d6861f13d7
Kostas <kmaneadis@gmail.com>
2019-11-13 23:40:08 +0200

    Logon functions logic, register first form data to db

--- KonstantinosManeadis/MainWindow.xaml.cs
+++ KonstantinosManeadis/MainWindow.xaml.cs
@@ -29,18 +29,37 @@ namespace KonstantinosManeadis
 
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine(sender.ToString(), "username: " + username_textbox.Text);
-            System.Diagnostics.Debug.WriteLine(sender.ToString(), "password: " + password_textbox.Password);
+            //Diagnostics.Debug.WriteLine(sender.ToString(), "username: " + username_textbox.Text);
+            //System.Diagnostics.Debug.WriteLine(sender.ToString(), "password: " + password_textbox.Password);
             try
             {
                 SqlConnection cn_connection = DB_Connection;
-                SqlCommand cmd_Command = new SqlCommand("insert into user_table(username, password) values(@username , @password)" , cn_connection);
+                SqlCommand cmd_Command = new SqlCommand("select password from [user_table] where username=@username" , cn_connection);
+
+                //SqlCommand cmd_Command = new SqlCommand("insert into user_table(username, password) values(@username , @password)" , cn_connection);
                 string username = username_textbox.Text;
                 cmd_Command.Parameters.AddWithValue("username", username);
-                string password = password_textbox.Password;
-                cmd_Command.Parameters.AddWithValue("password", password);
-
-                cmd_Command.ExecuteNonQuery();
+                using (SqlDataReader reader = cmd_Command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        string dbpass = reader["password"].ToString();
+                        if (dbpass == password_textbox.Password)
+                        {
+                            System.Diagnostics.Debug.WriteLine(sender.ToString(), "password match with given.. we continue the login process");
+                        }
+                        else
+                        {
+                            System.Diagnostics.Debug.WriteLine(sender.ToString(), "password is wrong");
+                        }
+                    }
+                    else
+                    {
+                        System.Diagnostics.Debug.WriteLine(sender.ToString(), "username doesnt exist to database");
+                    }
+                }
+                cn_connection.Close();
+                //cmd_Command.ExecuteNonQuery();
             }
             catch (Exception ex)
             {
@@ -61,9 +80,28 @@ namespace KonstantinosManeadis
 
         private void Register_Click(object sender, RoutedEventArgs e)
         {
-            
-            
-            
+            try
+            {
+                SqlConnection cn_connection = DB_Connection;
+                SqlCommand cmd_Command = new SqlCommand("insert into user_table(username, password) values(@username , @password)", cn_connection);
+                string username = username_textbox.Text;
+                cmd_Command.Parameters.AddWithValue("username", username);
+                string password = password_textbox.Password;
+                cmd_Command.Parameters.AddWithValue("password", password);
+
+                cmd_Command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            { 
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void username_textbox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
