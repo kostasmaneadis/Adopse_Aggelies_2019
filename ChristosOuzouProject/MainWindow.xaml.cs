88584729b85ea1b6c17fcb78c2fe6e184e358d1e
Christos Ouzounidis <chrisagioneri@yahoo.gr>
2019-11-28 14:48:09 +0200

    JobView,JobAdd

--- ChristosOuzouProject/MainWindow.xaml.cs
+++ ChristosOuzouProject/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,11 +29,13 @@ namespace ChristosOuzouProject
         static string constring = "SERVER=localhost;DATABASE=mydb;UID=christos;PASSWORD=2341093066;";
         MySqlConnection conn = new MySqlConnection(constring);
         List<BitmapImage> imgList = new List<BitmapImage>();
-;        public MainWindow()
+        int imagecounter = -1;
+        public MainWindow()
         {
             InitializeComponent();
             CB_fill();
-
+            ViewJob vj = new ViewJob();
+            vj.Show();
         }
         private void CB_fill()
         {
@@ -41,12 +44,12 @@ namespace ChristosOuzouProject
             try
             {
                 conn.Open();
-                MySqlCommand comm = new MySqlCommand("SELECT DISTINCT name FROM subcategories WHERE catId=1", conn);
+                MySqlCommand comm = new MySqlCommand("SELECT title FROM category WHERE parentId=1", conn);
                 MySqlDataReader rdr = comm.ExecuteReader();
                 while (rdr.Read())
-                    data.Add(rdr["name"].ToString());
+                    data.Add(rdr["title"].ToString());
                 rdr.Close();
-                mainCatCB.ItemsSource=data;
+                mainCatCB.ItemsSource = data;
                 conn.Close();
             }
             catch (MySqlException ex)
@@ -73,23 +76,26 @@ namespace ChristosOuzouProject
         }
         public void Subcat_fill(object sender, EventArgs e)
         {
-            try {
+            try
+            {
                 List<string> data = new List<string>();
                 conn.Open();
-                MySqlCommand comm = new MySqlCommand("SELECT sub2 FROM subcategories WHERE name LIKE @par1",conn);
+                MySqlCommand comm = new MySqlCommand("SELECT title FROM category WHERE parentId = (SELECT categoryId FROM category WHERE title LIKE @par1)", conn);
                 comm.Parameters.AddWithValue("@par1", mainCatCB.Text);
                 MySqlDataReader rdr = comm.ExecuteReader();
                 while (rdr.Read())
-                    data.Add(rdr["sub2"].ToString());
+                    data.Add(rdr["title"].ToString());
                 rdr.Close();
                 subCatCB.ItemsSource = data;
                 conn.Close();
             }
-            catch (MySqlException ex){
+            catch (MySqlException ex)
+            {
                 System.Diagnostics.Debug.WriteLine(ex.ToString());
             }
-            
+
         }
+
         public void City_fill(object sender, EventArgs e)
         {
             try
@@ -146,11 +152,112 @@ namespace ChristosOuzouProject
                 foreach (string filename in op.FileNames)
                     imgList.Add(new BitmapImage(new Uri(filename)));
             }
+            uploadImg.Source = imgList.First();
+            uploadImg.Visibility = Visibility.Visible;
+            rmB.Visibility = Visibility.Visible;
+        }
+
+        private void nextPhoto(object sender, RoutedEventArgs e)
+        {
+            Button bt = e.Source as Button;
+            if (bt.Name == "nextBT")
+                if (imagecounter >= (imgList.Count - 1))
+                    imagecounter = 0;
+                else
+                    imagecounter++;
+            else if (bt.Name == "beforeBT")
+                if (imagecounter <= 0)
+                    imagecounter = imgList.Count - 1;
+                else
+                    imagecounter--;
+            uploadImg.Source = imgList.ElementAt(imagecounter);
 
         }
 
-    }
+        private void RemoveImages(object sender, RoutedEventArgs e)
+        {
+            imgList.Clear();
+            uploadImg.Visibility = Visibility.Hidden;
+        }
 
+
+
+        private void CreateJobAd(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int adid = 0;
+                conn.Open();
+                MySqlCommand comm = new MySqlCommand("SELECT id FROM addresses WHERE(address LIKE @address AND municipality LIKE @mun AND region LIKE @reg)", conn);
+                comm.Parameters.AddWithValue("@address", addressCB.SelectedValue.ToString());
+                comm.Parameters.AddWithValue("@mun", cityCB.SelectedValue.ToString());
+                comm.Parameters.AddWithValue("@reg", regionCB.SelectedValue.ToString());
+                MySqlDataReader rdr = comm.ExecuteReader();
+                if (rdr.Read())
+                    adid = Convert.ToInt32(rdr["id"]);
+                rdr.Close();
+                comm = new MySqlCommand("INSERT INTO fulladdress(addressid, number) VALUES( @adid,@number)", conn);
+                comm.Parameters.AddWithValue("@adid", adid);
+                comm.Parameters.AddWithValue("@number", addressNumberTB.Text);
+                comm.ExecuteNonQuery();
+                long adrId = comm.LastInsertedId;
+                comm = new MySqlCommand("" +
+                    "INSERT INTO Ads(userId, dateAdded,description,superAd,address) VALUES" +
+                    "( @userId, @date, @descr,false,@address)", conn);
+                comm.Parameters.AddWithValue("@userId", 1);
+                comm.Parameters.AddWithValue("@date", DateTime.Now.ToString("yyyy-MM-dd"));
+                comm.Parameters.AddWithValue("@descr", DescriptionTB.Text);
+                comm.Parameters.AddWithValue("@address", adrId);
+                comm.ExecuteNonQuery();
+                long adId = comm.LastInsertedId;
+                comm = new MySqlCommand("INSERT INTO propertyvalue VALUES(@adid,1,@fptime)", conn);
+                comm.Parameters.AddWithValue("@adid", adId);
+                ComboBoxItem cmbItem = foul_part_timeCB.SelectedItem as ComboBoxItem;
+                comm.Parameters.AddWithValue("@fptime", cmbItem.Content.ToString());
+                comm.ExecuteNonQuery();
+                comm = new MySqlCommand("INSERT INTO propertyvalue VALUES(@adid,2,@salary)", conn);
+                comm.Parameters.AddWithValue("@adid", adId);
+                comm.Parameters.AddWithValue("@salary", salaryTB.Text);
+                comm.ExecuteNonQuery();
+                if (imgList.Count > 0)
+                {
+                    foreach (BitmapImage i in imgList)
+                    {
+                        comm = new MySqlCommand("INSERT INTO photos(adsId,photo) VALUES(@adid,@photo)", conn);
+                        byte[] data = null;
+                        /* JpegBitmapEncoder encoder = new JpegBitmapEncoder();
+                         System.Diagnostics.Debug.WriteLine("ok1");
+                         encoder.Frames.Add(BitmapFrame.Create(i);
+                         System.Diagnostics.Debug.WriteLine("ok2");
+                         using (MemoryStream ms = new MemoryStream())
+                         {
+                             System.Diagnostics.Debug.WriteLine("ok3");
+                             encoder.Save(ms);
+                             System.Diagnostics.Debug.WriteLine("ok4");
+                             data = ms.ToArray();
+                             System.Diagnostics.Debug.WriteLine("ok");
+                         }*/
+                        System.Diagnostics.Debug.WriteLine(i.ToString());
+                        var ms = new MemoryStream();
+                        var jpgEncoder = new JpegBitmapEncoder();
+                        jpgEncoder.Frames.Add(BitmapFrame.Create(i));
+                        jpgEncoder.Save(ms);
+                        data = ms.GetBuffer();
+                        comm.Parameters.AddWithValue("@adid", adId);
+                        comm.Parameters.AddWithValue("@photo", data);
+                        comm.ExecuteNonQuery();
+                    }
+                    conn.Close();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+            
+        }
+    }
 }
+
     
 
---
+++ ChristosOuzouProject/ViewJob.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChristosOuzouProject
+{
+    /// <summary>
+    /// Interaction logic for ViewJob.xaml
+    /// </summary>
+    public partial class ViewJob : Window
+    {
+        public ViewJob()
+        {
+            InitializeComponent();
+           
+        }
+        private void fillad(MySqlDataReader rdr)
+        {
+            if (rdr.Read())
+            {
+               // jobTitleL.Content = 
+                jobLocationL.Content = rdr["region"].ToString() + ", " + rdr["municipality"].ToString();
+                 jobAddressL.Content= rdr["address"].ToString() + ", " + rdr["number"].ToString();
+                jobKindL.Content= rdr["title"].ToString();
+                jobSalaryL.Content = rdr["salary"].ToString();
+                descriptionTB.Text = rdr["description"].ToString();
+            }
+        }
+    }
+}
