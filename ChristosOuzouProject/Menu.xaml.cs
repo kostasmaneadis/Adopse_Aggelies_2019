d0298995a84f5ab59c633fcfe8b64d54a226c68d
kostasmaneadis <kmaneadis@gmail.com>
2020-01-12 22:13:25 +0200

    Adding general menus for jobs and cars

---
+++ ChristosOuzouProject/Menu.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChristosOuzouProject
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+        private void newAdButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow newAdpage = new MainWindow();
+            NavigationService.Navigate(newAdpage);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ViewJob showAdsPanelPage = new ViewJob();
+            NavigationService.Navigate(showAdsPanelPage);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+             
+        }
+    }
+}
---
+++ ChristosOuzouProject/Menu_cars.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChristosOuzouProject
+{
+    /// <summary>
+    /// Interaction logic for Menu_cars.xaml
+    /// </summary>
+    public partial class Menu_cars : Page
+    {
+        public Menu_cars()
+        {
+            InitializeComponent();
+        }
+        private void newAdButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddCar newAdpage = new AddCar();
+            NavigationService.Navigate(newAdpage);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ViewCar showAdsPanelPage = new ViewCar();
+            NavigationService.Navigate(showAdsPanelPage);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
