452b6db51d726fa8d5f104305c2642d5cc863ae9
kostasmaneadis <kmaneadis@gmail.com>
2020-01-12 18:19:29 +0200

    Managers Panel, add new actions and tabs for real estate ads

--- KonstantinosManeadis/Administrator/Administrator_main.xaml.cs
+++ KonstantinosManeadis/Administrator/Administrator_main.xaml.cs
@@ -44,95 +44,7 @@ namespace KonstantinosManeadis.Administrator
             }
         }
 
-        private void User_Action_Button_Click(object sender, RoutedEventArgs e)
-        {
-                string action = users_action.Text;
-                if (action == "select") 
-                {
-                    try
-                    {
-                        MySqlConnection connection = new MySqlConnection(static_connectionString);
-                        connection.Open();
-                        MySqlCommand command = new MySqlCommand("select * from users where userid=@id", connection);
-                        string id = user_id_textbox.Text;
-                        command.Parameters.AddWithValue("id", id);
-                        using (MySqlDataReader reader = command.ExecuteReader())
-                        {
-                            if (reader.Read())
-                            {
-                                string output = "";
-                                output = reader["username"].ToString();
-                                output += " " + reader["firstname"].ToString();
-                                output += " " + reader["lastname"].ToString();
-                                output += " " + reader["birthday"].ToString();
-                                output += " " + reader["email"].ToString();
-                                users_label_status.Content = output;
-                            }
-                            else
-                            {
-                                //Username_label.Content = "You must Login to Continue to this page";
-                            }
-                        }
-                        connection.Close();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                        users_label_status.Content = "Something went wrong";
-                    }
-                }
-                else if (action == "update")
-                {
-                    try
-                    {
-                        MySqlConnection connection = new MySqlConnection(static_connectionString);
-                        connection.Open();
-                        string column = user_table_column.Text;
-                        MySqlCommand command = new MySqlCommand("UPDATE `users` SET `"+column+"`=@value Where `userid`=@id ", connection);
-                        string id = user_id_textbox.Text;
-                        command.Parameters.AddWithValue("id", id);
-                        
-                        //command.Parameters.AddWithValue("column", "`"+column+"`");
-                        string value = user_textbox_new_value.Text;
-                        command.Parameters.AddWithValue("value", value);
-                        command.ExecuteNonQuery();
-                        connection.Close();
-                        users_label_status.Content = "Column " +column+ " updated for user " +id+ " with value ->" + value;
-                     }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                        users_label_status.Content = "Something went wrong";
-                    }
-                }
-                else if (action == "delete")
-                {
-                    try
-                    {
-                        MySqlConnection connection = new MySqlConnection(static_connectionString);
-                        connection.Open();
-                        
-                        MySqlCommand command = new MySqlCommand("DELETE from `users` Where `userid`=@id ", connection);
-                        string id = user_id_textbox.Text;
-                        command.Parameters.AddWithValue("id", id);
-
-                        command.ExecuteNonQuery();
-                        connection.Close();
-                        users_label_status.Content = "User " +id+ " Deleted Successfully";
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show(ex.Message);
-                        users_label_status.Content = "Something went wrong";
-                    }
-                }
-                else
-                {
-                     users_label_status.Content = "Something went wrong";
-                }
-                
-            }
-
+        
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             if (e.Source is TabControl)
@@ -189,6 +101,182 @@ namespace KonstantinosManeadis.Administrator
 
             }
         }
+        private void User_Action_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string action = users_action.Text;
+            if (action == "select")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand("select * from users where userid=@id", connection);
+                    string id = user_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string output = "";
+                            output = reader["username"].ToString();
+                            output += " " + reader["firstname"].ToString();
+                            output += " " + reader["lastname"].ToString();
+                            output += " " + reader["birthday"].ToString();
+                            output += " " + reader["email"].ToString();
+                            users_label_status.Content = output;
+                        }
+                        else
+                        {
+                            //Username_label.Content = "You must Login to Continue to this page";
+                        }
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    users_label_status.Content = "Something went wrong";
+                }
+            }
+            else if (action == "update")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+                    string column = user_table_column.Text;
+                    MySqlCommand command = new MySqlCommand("UPDATE `users` SET `" + column + "`=@value Where `userid`=@id ", connection);
+                    string id = user_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+
+                    //command.Parameters.AddWithValue("column", "`"+column+"`");
+                    string value = user_textbox_new_value.Text;
+                    command.Parameters.AddWithValue("value", value);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    users_label_status.Content = "Column " + column + " updated for user " + id + " with value ->" + value;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    users_label_status.Content = "Something went wrong";
+                }
+            }
+            else if (action == "delete")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+
+                    MySqlCommand command = new MySqlCommand("DELETE from `users` Where `userid`=@id ", connection);
+                    string id = user_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    users_label_status.Content = "User " + id + " Deleted Successfully";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    users_label_status.Content = "Something went wrong";
+                }
+            }
+            else
+            {
+                users_label_status.Content = "Something went wrong";
+            }
+
+        }
+        private void Ads_Action_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string action = ads_action.Text;
+            if (action == "select")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand("select * from ads where userid=@id", connection);
+                    string id = ads_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string output = "";
+                            output = " User ID:" + reader["userId"].ToString();
+                            output += " Description: " + reader["description"].ToString();
+                            output += " Category: " + reader["categoryId"].ToString();
+                            output += " SuperAd: " + reader["superAd"].ToString();
+                            output += " Address: " + reader["address"].ToString();
+                            ads_label_status.Content = output;
+                        }
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    ads_label_status.Content = "Something went wrong";
+                }
+            }
+            else if (action == "update")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+                    string column = ads_table_column.Text;
+                    MySqlCommand command = new MySqlCommand("UPDATE `ads` SET `" + column + "`=@value Where `adId`=@id ", connection);
+                    string id = ads_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+
+                    string value = ads_textbox_new_value.Text;
+                    command.Parameters.AddWithValue("value", value);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    ads_label_status.Content = "Column " + column + " updated for Ad " + id + " with value ->" + value;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    ads_label_status.Content = "Something went wrong";
+                }
+            }
+            else if (action == "delete")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+
+                    MySqlCommand command = new MySqlCommand("DELETE FROM propertyvalue WHERE adId=@id", connection);
+                    string id = ads_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+                    command.ExecuteNonQuery();
+
+                    command = new MySqlCommand("DELETE FROM ads WHERE adId=@id", connection);
+                    command.Parameters.AddWithValue("id", id);
+                    command.ExecuteNonQuery();
+
+                    connection.Close();
+                    ads_label_status.Content = "Ad " + id + " Deleted Successfully";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    ads_label_status.Content = "Something went wrong";
+                }
+            }
+            else
+            {
+                ads_label_status.Content = "Something went wrong";
+            }
+        }
+
+
     }
-    }
+}
 
--- KonstantinosManeadis/Manager/Manager_main.xaml.cs
+++ KonstantinosManeadis/Manager/Manager_main.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using MySql.Data.MySqlClient;
+using System.Data;
 namespace KonstantinosManeadis.Manager
 {
     /// <summary>
@@ -18,11 +10,12 @@ namespace KonstantinosManeadis.Manager
     /// </summary>
     public partial class Manager_main : Page
     {
+        private static String static_connectionString = Settings1.Default.connectionString;
         public Manager_main()
         {
             InitializeComponent();
             string LogedInUserRole = KonstantinosManeadis.Login_page.GetUserRole();
-            if (LogedInUserRole != "administrator" )
+            if (LogedInUserRole != "administrator")
             {
                 if (LogedInUserRole != "manager")
                 {
@@ -33,7 +26,223 @@ namespace KonstantinosManeadis.Manager
         }
         private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            if (e.Source is TabControl)
+            {
+                if (users_tab.IsSelected)
+                {
+                    //users
+                    try
+                    {
+                        MySqlConnection connection = new MySqlConnection(static_connectionString);
+                        connection.Open();
+                        MySqlCommand command = new MySqlCommand("select * from users", connection);
+                        MySqlDataAdapter user_adapter = new MySqlDataAdapter(command);
+                        DataTable dt = new DataTable("users");
 
+                        user_adapter.Fill(dt);
+                        Users_datatable.ItemsSource = dt.DefaultView;
+                        user_adapter.Update(dt);
+                        
+                        
+                        connection.Close();
+                       
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+                if (aggelies_tab.IsSelected)
+                {
+                    try
+                    {
+                        MySqlConnection connection = new MySqlConnection(static_connectionString);
+                        connection.Open();
+                        MySqlCommand command = new MySqlCommand("select * from ads", connection);
+                        MySqlDataAdapter user_adapter = new MySqlDataAdapter(command);
+                        DataTable dt = new DataTable("ads");
+                        user_adapter.Fill(dt);
+                        Ads_datatable.ItemsSource = dt.DefaultView;
+                        user_adapter.Update(dt);
+                        connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+                if (actions_tab.IsSelected)
+                {
+
+                }
+               
+                if (addresses_tab.IsSelected)
+                {
+                    try
+                    {
+                        MySqlConnection connection = new MySqlConnection(static_connectionString);
+                        connection.Open();
+                        MySqlCommand command = new MySqlCommand("select * from addresses", connection);
+                        MySqlDataAdapter addresses_adapter = new MySqlDataAdapter(command);
+                        DataTable dt = new DataTable("addresses");
+
+                        addresses_adapter.Fill(dt);
+                        Addresses_datatable.ItemsSource = dt.DefaultView;
+                        addresses_adapter.Update(dt);
+
+
+                        connection.Close();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+            }
+        }
+
+        
+        private void Ads_Action_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string action = ads_action.Text;
+            if (action == "select")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+                    MySqlCommand command = new MySqlCommand("select * from ads where userid=@id", connection);
+                    string id = ads_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            string output = "";
+                            output = " User ID:" + reader["userId"].ToString();
+                            output += " Description: " + reader["description"].ToString();
+                            output += " Category: " + reader["categoryId"].ToString();
+                            output += " SuperAd: " + reader["superAd"].ToString();
+                            output += " Address: " + reader["address"].ToString();
+                            ads_label_status.Content = output;
+                        }
+                    }
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    ads_label_status.Content = "Something went wrong";
+                }
+            }
+            else if (action == "update")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+                    string column = ads_table_column.Text;
+                    MySqlCommand command = new MySqlCommand("UPDATE `ads` SET `" + column + "`=@value Where `adId`=@id ", connection);
+                    string id = ads_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+
+                    string value = ads_textbox_new_value.Text;
+                    command.Parameters.AddWithValue("value", value);
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    ads_label_status.Content = "Column " + column + " updated for Ad " + id + " with value ->" + value;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    ads_label_status.Content = "Something went wrong";
+                }
+            }
+            else if (action == "delete")
+            {
+                try
+                {
+                    MySqlConnection connection = new MySqlConnection(static_connectionString);
+                    connection.Open();
+
+                    MySqlCommand command = new MySqlCommand("DELETE FROM propertyvalue WHERE adId=@id", connection);
+                    string id = ads_id_textbox.Text;
+                    command.Parameters.AddWithValue("id", id);
+                    command.ExecuteNonQuery();
+
+                    command = new MySqlCommand("DELETE FROM ads WHERE adId=@id", connection);
+                    command.Parameters.AddWithValue("id", id);
+                    command.ExecuteNonQuery();
+
+                    connection.Close();
+                    ads_label_status.Content = "Ad " + id + " Deleted Successfully";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    ads_label_status.Content = "Something went wrong";
+                }
+            }
+            else
+            {
+                ads_label_status.Content = "Something went wrong";
+            }
+        }
+
+        private void Address_Action_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(static_connectionString);
+                connection.Open();
+                MySqlCommand command = new MySqlCommand("insert into `fulladdress` (`addressid`,`number`) values(@address_id, @number_id) ", connection);
+                string address_id = address_id_textbox.Text;
+                command.Parameters.AddWithValue("address_id", address_id);
+
+                string number_id = numbers_id_textbox.Text;
+                command.Parameters.AddWithValue("number_id", number_id);
+
+                command.ExecuteNonQuery();
+                connection.Close();
+                addresses_label_status.Content = "New Address and Number Inserted : " +address_id + " " + number_id ;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                addresses_label_status.Content = "Something went wrong";
+            }
+        }
+        //ori_Address_Action_Button_Click
+        private void ori_Address_Action_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(static_connectionString);
+                connection.Open();
+                string column = ads_table_column.Text;
+                MySqlCommand command = new MySqlCommand("insert into `addresses` (`address`, `municipality`, `region`) values(@address, @municipality, @region) ", connection);
+                string address = ori_address_name_textbox.Text;
+                command.Parameters.AddWithValue("address", address);
+
+                string municipality = ori_address_municipality_textbox.Text;
+                command.Parameters.AddWithValue("municipality", municipality);
+
+                string region = ori_address_region_textbox.Text;
+                command.Parameters.AddWithValue("region", region);
+
+                command.ExecuteNonQuery();
+                connection.Close();
+                ori_addresses_label_status.Content = "New General Address Inserted : " + address + " - " + municipality  + " - " + region;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                ori_addresses_label_status.Content = "Something went wrong";
+            }
         }
     }
-}
+    }
